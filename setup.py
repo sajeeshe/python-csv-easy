c047dff50e6adef583eb265c75e315b3aee9e81a
Sajeesh <sajeeshe@suventure.in>
2017-07-14 21:34:24 +0530

    CSV to DB

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='pycsvmadeeasy',
+setup(name='pycsv',
       version='1.7',
       description='Reading CSV file and insert into database',
       url='https://github.com/sajeeshe/python-csv-easy',
       author='Sajeesh E Namboothiri',
       author_email='sajeeshe@gmail.com',
       license='MIT',
-      packages=['pycsvmadeeasy'],
+      packages=['pycsv'],
       keywords='CSV database pandas',
       zip_safe=False)
