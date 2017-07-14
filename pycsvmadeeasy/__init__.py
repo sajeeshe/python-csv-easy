4abe07735739923e75daca4269fb649028bab00b
Sajeesh <sajeeshe@suventure.in>
2017-07-14 19:44:35 +0530

    py csv

---
+++ pycsvmadeeasy/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return "Hello how are you"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycsvmadeeasy',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sajeeshe/python-csv-easy',
+      author='Python Circus',
+      author_email='sajeeshe@gmail.com',
+      license='MIT',
+      packages=['pycsvmadeeasy'],
+      zip_safe=False)
